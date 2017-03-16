a07c2b14981d79cb2fe8eebc20d1e85dcc1731c5
yfzhang_nk <yfzhang_nk@qq.com>
2017-03-16 20:22:37 +0800

    add search a 2D Matrix

---
+++ solutions/240_Search_a_2D_Matrix_II/240_search_a_2d_matrix_ii.cpp
@@ -0,0 +1,25 @@
+/**
+ * author: 一元硬币
+ * date: 2017-03-15
+ * problem: 240. Search a 2D Matrix II 
+ */
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        if (n == 0) return false;
+        int m = matrix[0].size();
+        if (m == 0) return false;
+        int base_i = 0, base_j = m - 1;
+        while (base_i < n && base_j >= 0) {
+            if (matrix[base_i][base_j] == target) {
+                return true;
+            } else if (matrix[base_i][base_j] < target) {
+                base_i++;
+            } else {
+                base_j--;
+            }
+        }
+        return false;
+    }
+};
